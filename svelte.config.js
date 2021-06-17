1ee892419421068a32c35933ed78d95212e5c54c
Parkar99 <nabeelparkar99@gmail.com>
2021-06-17 19:42:56 +0530

    add: Svelte static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+		target: '#svelte',
+		adapter: adapter(),
+	},
+}
 
-export default config;
+export default config
