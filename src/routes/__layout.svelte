fbfb672454d410258e73a37438efa5e450f4acc0
Parkar99 <nabeelparkar99@gmail.com>
2021-06-17 19:56:45 +0530

    add: title to html

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,10 @@
   import '../app.scss'
 </script>
 
+<svelte:head>
+  <title>Nabeel's Keyboard</title>
+</svelte:head>
+
 <div class="w-3/4 h-screen mx-auto">
   <div class="h-full flex flex-col justify-center">
     <div class="flex">
